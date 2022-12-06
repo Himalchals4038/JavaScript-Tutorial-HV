0dfd62e5c429443382d47a88d3f65534f87b48f8
Himalchals4038 <hrivu786@gmail.com>
2022-12-06 15:06:30 +0530

    First Modified Upload 	modified:   Chapter 2/How JavaScript Works/hjw_05_mixed.js

--- Chapter 2/How JavaScript Works/hjw_05_mixed.js
+++ Chapter 2/How JavaScript Works/hjw_05_mixed.js
@@ -21,9 +21,21 @@ const myFunc = function(){
 // 1. firstName variable is formed but it will be in TDZ before execution of line 2.
 // 2. Second name variable is formed but it will be in TDZ before execution of line 3.
 // 3. myFunc function is created and its content is stored in the global scope.
-// 4. Inside the myFunc function var1 variable is created but stored with no value.
-// 5. Inside the myFunc function var2 variable is created but stored with no value.
 
 // Code Execution Phase :-
+// 1. Hello World is printed as output.
+// 2. firstName is initialized and assigned a constant Rajeev.
+// 3. lastName is initialized and assigned a constant Sharma.
+// 4. Function Execution Context :-
+
+// Local Memory Context Phase :
+// a. Inside the myFunc function var1 variable is created but uninitialized.
+// b. Inside the myFunc function var2 variable is created but uninitialized.
+
+// Local Code Execution Phase :
+// a. var1 is initialized and alloted a value "First Variable".
+// b. var2 is initialized and alloted a value "Second Variable".
+// c. The value of var1 is printed.
+// d. The value of var2 is printed.
 
 
